dd1dcca5c83675d3f47e525ccf7d739b19234d3f
Kieu <kieuphan530@gmail.com>
2021-09-21 21:58:27 -0500

    commit tree 9da8bc8a02914f5e4a606b0b0cc8ef91b2dbc6a4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code in this file!
+const currentUser = 'Kathy Phan';
+
+// let welcomeMessage = 'Welcome to Flatbook, !';
+
+// let welcomeMessage = currentUser;
+
+// let welcomeMessage = 'Welcome to Flatbook, currentUser';
+
+// let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log(excitedWelcomeMessage);
+
+// let shortGreeting = 'Welcome, ';
+// let shortGreeting = `Welcome, ${currentUser}`;
+// const firstInitial = currentUser[0];
+// const restOfName = currentUser.slice(6);
+
+// shortGreeting = 'Welcome, ' + firstInitial + restOfName + '!';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
